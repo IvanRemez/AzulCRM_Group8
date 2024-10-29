205ef676f3019b73aeeaa0ac2e95b8354a5ad567
kenanhs24 <170270713+kenanhs24@users.noreply.github.com>
2024-10-28 21:12:44 -0400

    i just create a step def. class and I did not implement y code yet.

---
+++ src/test/java/com/azulCRM/step_definitions/Logout_step_defs.java
@@ -0,0 +1,29 @@
+package com.azulCRM.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Logout_step_defs {
+
+    @Given("the user is logged into the application")
+    public void the_user_is_logged_into_the_application() {
+
+    }
+    @When("the user clicks on the profile icon")
+    public void the_user_clicks_on_the_profile_icon() {
+
+    }
+    @When("the user selects the {string} option")
+    public void the_user_selects_the_option(String string) {
+
+    }
+    @Then("the user should be logged out of the application")
+    public void the_user_should_be_logged_out_of_the_application() {
+
+    }
+    @Then("the user should see the login screen")
+    public void the_user_should_see_the_login_screen() {
+
+    }
+}
