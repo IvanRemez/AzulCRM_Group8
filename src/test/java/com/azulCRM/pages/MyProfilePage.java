c3bb8bf33ef8ab949ab037baff40d05e09350c9d
paolaguacho <Paolacristina111@gmail.com>
2024-10-31 14:12:41 -0400

    Updated ProfilePageStepDefs.java

---
+++ src/test/java/com/azulCRM/pages/MyProfilePage.java
@@ -0,0 +1,40 @@
+package com.azulCRM.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MyProfilePage extends BasePage {
+
+@FindBy(id="user-block")
+public WebElement userBlock;
+
+@FindBy(xpath="//span[.='My Profile']")
+    public WebElement myProfile;
+
+@FindBy(xpath="//div[@id='profile-menu-filter']")
+    public WebElement allTabs;
+
+@FindBy(xpath="//div[@id='profile-menu-filter']/a[.='General']")
+public WebElement generalTab;
+
+@FindBy(xpath = "//div[@id='profile-menu-filter']/a[normalize-space(text())='Tasks']")
+public WebElement tasksTab;
+
+@FindBy(xpath = "//div[@id='profile-menu-filter']/a[normalize-space(text())='Calendar']")
+public WebElement calendarTab;
+
+@FindBy(xpath = "//div[@id='profile-menu-filter']/a[normalize-space(text())='Conversations']")
+public WebElement conversationsTab;
+
+
+public String generalTabDisplay(){
+    return allTabs.getText();
+}
+
+
+
+
+
+
+
+}
