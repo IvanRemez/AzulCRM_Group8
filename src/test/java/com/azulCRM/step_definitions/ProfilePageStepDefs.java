6a91d06be1c1cc901316fa1dcfa9260746bb397b
paolaguacho <Paolacristina111@gmail.com>
2024-11-04 14:45:48 -0500

    some update

--- src/test/java/com/azulCRM/step_definitions/ProfilePageStepDefs.java
+++ src/test/java/com/azulCRM/step_definitions/ProfilePageStepDefs.java
@@ -1,61 +1,20 @@
 package com.azulCRM.step_definitions;
 
-import com.azulCRM.pages.LoginPage;
-import com.azulCRM.pages.MyProfilePage;
-import com.azulCRM.utilities.BrowserUtils;
-import com.azulCRM.utilities.Driver;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.azulCRM.pages.LoginPage;
+import io.cucumber.java.en.Given;
 
 public class ProfilePageStepDefs {
 
-    //Creating object class
-    MyProfilePage myProfilePage = new MyProfilePage();
     LoginPage loginPage = new LoginPage();
 
-    @Given ("the user is logged in")
-public void the_user_is_logged_in() {
-    loginPage.login("helpdesk101@Cydeo.com","UserUser");
+@Given("the user is logged in")
+    public void the_user_is_logged_in() {
+    loginPage.userName.sendKeys("helpdesk101@Cydeo.com");
+    loginPage.password.sendKeys("UserUser");
+    loginPage.submit.click();
+
 }
-
-    @When("user clicks on user block")
-    public void userClicksOnUserBlock() {
-        myProfilePage.userBlock.click();
-    }
-
-    @And("user clicks on My Profile")
-    public void userClicksOnMyProfile() {
-        myProfilePage.myProfile.click();
-    }
-
-    @Then("User is on My Profile page")
-    public void userIsOnMyProfilePage() {
-
-    }
-
-    @When("User views the general tab menu")
-    public void user_views_the_general_tab_menu() {
-        myProfilePage.allTabs.isDisplayed();
-    }
-
-    @Then("User sees the following tab options:")
-    public void user_sees_the_following_tab_options(List<String> expectedTab) {
-
-        List<String> actualTabs = BrowserUtils.getElementsText(By.xpath("//div[@id='profile-menu-filter']/a"));
-
-        Assert.assertEquals(actualTabs, expectedTab);
-
-    }
-
 
 
 
