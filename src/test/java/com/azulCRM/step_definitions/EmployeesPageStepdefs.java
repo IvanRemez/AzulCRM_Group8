57a25a5e49bf8e6f791d64d41199237eb1d79f94
Dianaa <170193956+sunray202S@users.noreply.github.com>
2024-11-03 19:37:38 -0500

    I created EmployeesPage StepDef

---
+++ src/test/java/com/azulCRM/step_definitions/EmployeesPageStepdefs.java
@@ -0,0 +1,32 @@
+package com.azulCRM.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EmployeesPageStepdefs {
+    @Given("User logged in as {string}")
+    public void userLoggedInAs(String arg0) {
+    }
+
+    @And("User on the Base Page")
+    public void userOnTheBasePage() {
+    }
+
+    @When("User sees Employees page button")
+    public void userSeesEmployeesPageButton() {
+    }
+
+    @And("User clicks Employees page button")
+    public void userClicksEmployeesPageButton() {
+    }
+
+    @Then("User sees following eight modules in the Employees page:")
+    public void userSeesFollowingEightModulesInTheEmployeesPage() {
+    }
+
+    @Then("User sees the Company Structure by default")
+    public void userSeesTheCompanyStructureByDefault() {
+    }
+}
