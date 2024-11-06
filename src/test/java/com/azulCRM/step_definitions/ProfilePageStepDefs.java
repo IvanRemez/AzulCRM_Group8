c258c91b2a2633e1b2a1073fda91f64b1e584567
paolaguacho <Paolacristina111@gmail.com>
2024-11-06 16:42:47 -0500

    Update to MyProfilePageStepDefs.java

--- src/test/java/com/azulCRM/step_definitions/ProfilePageStepDefs.java
+++ src/test/java/com/azulCRM/step_definitions/ProfilePageStepDefs.java
@@ -5,6 +5,7 @@ import com.azulCRM.pages.LoginPage;
 import com.azulCRM.pages.MyProfilePage;
 import com.azulCRM.utilities.BrowserUtils;
 import com.azulCRM.utilities.ConfigurationReader;
+import com.azulCRM.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -39,10 +40,21 @@ public class ProfilePageStepDefs {
 
     @Then("user sees the following general options tabs:")
     public void userSeesTheFollowingGeneralOptionsTabs(List<String> expectedOptions) {
+                    //Polymorphism: down-casting List<Elements> to List<String>
         List <String> actualOptions = BrowserUtils.getElementsText(myProfilePage.allTabs);
         Assert.assertEquals(expectedOptions, actualOptions);
 
     }
 
+    @When("user lands on Profile Page")
+    public void userLandsOnProfilePage() {
+        String userEmail = myProfilePage.contactInfoEmail.getText();
+        Assert.assertEquals(Driver.getDriver().getTitle(), userEmail);
+    }
 
+
+    @Then("user sees email address under the General tab")
+    public void userSeesEmailAddressUnderTheGeneralTab() {
+        Assert.assertTrue(myProfilePage.contactInfoEmail.isDisplayed());
+    }
 }
