14c3d35b67e9b988ff74c633d9dd00a01972c181
paolaguacho <Paolacristina111@gmail.com>
2024-11-06 15:00:47 -0500

    Updated MyProfilePage

--- src/test/java/com/azulCRM/pages/MyProfilePage.java
+++ src/test/java/com/azulCRM/pages/MyProfilePage.java
@@ -3,22 +3,13 @@ package com.azulCRM.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class MyProfilePage {
-    @FindBy(id="user-block")
-    public WebElement userBlock;
+import java.util.List;
 
-    @FindBy(xpath="//span[.='My Profile']")
-    public WebElement myProfile;
+public class MyProfilePage extends BasePage {
+
+
     @FindBy(xpath="//div[@id='profile-menu-filter']/a")
-    public WebElement allTabs;
-    @FindBy(xpath="//div[@id='profile-menu-filter']/a[.='General']")
-    public WebElement generalTab;
-    @FindBy(xpath = "//div[@id='profile-menu-filter']/a[normalize-space(text())='Tasks']")
-    public WebElement tasksTab;
-    @FindBy(xpath = "//div[@id='profile-menu-filter']/a[normalize-space(text())='Calendar']")
-    public WebElement calendarTab;
-    @FindBy(xpath = "//div[@id='profile-menu-filter']/a[normalize-space(text())='Conversations']")
-    public WebElement conversationsTab;
+    public List<WebElement> allTabs;
 
 
 }
